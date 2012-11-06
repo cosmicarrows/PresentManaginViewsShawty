ac3c9bdbf866a284080d7cb9048b943e877f90ca
Laurence Wingo <laurencewingo@Laurences-MacBook-Pro.local>
2012-11-06 15:57:29 -0500

    Initial Commit

---
+++ PresentManaginViewsShawtyTests/PresentManaginViewsShawtyTests.h
@@ -0,0 +1,13 @@
+//
+//  PresentManaginViewsShawtyTests.h
+//  PresentManaginViewsShawtyTests
+//
+//  Created by Laurence Wingo on 11/6/12.
+//  Copyright (c) 2012 Laurence Wingo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PresentManaginViewsShawtyTests : SenTestCase
+
+@end
---
+++ PresentManaginViewsShawtyTests/PresentManaginViewsShawtyTests.m
@@ -0,0 +1,32 @@
+//
+//  PresentManaginViewsShawtyTests.m
+//  PresentManaginViewsShawtyTests
+//
+//  Created by Laurence Wingo on 11/6/12.
+//  Copyright (c) 2012 Laurence Wingo. All rights reserved.
+//
+
+#import "PresentManaginViewsShawtyTests.h"
+
+@implementation PresentManaginViewsShawtyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PresentManaginViewsShawtyTests");
+}
+
+@end
